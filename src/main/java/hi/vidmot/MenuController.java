c9eae4ef7a737e2302b3b62745f3383627472000
Reynirjr <brj46@hi.is>
2024-03-19 22:58:43 +0000

    leyfir notanda að loka forritinu frá menu bar

--- src/main/java/hi/vidmot/MenuController.java
+++ src/main/java/hi/vidmot/MenuController.java
@@ -2,7 +2,11 @@ package hi.vidmot;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.RadioMenuItem;
+
+import java.util.Optional;
 
 public class MenuController {
     @FXML
@@ -46,5 +50,19 @@ public class MenuController {
     }
 
     public void onQuit(ActionEvent actionEvent) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Hætta");
+        alert.setHeaderText("Beiðni um að hætta");
+        alert.setContentText("Ertu viss um að þú viljir hætta?");
+
+        ButtonType buttonTypeYes = new ButtonType("Já");
+        ButtonType buttonTypeNo = new ButtonType("Nei");
+        alert.getButtonTypes().setAll(buttonTypeYes, buttonTypeNo);
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.isPresent() && result.get() == buttonTypeYes) {
+            System.exit(0);
+        }
+
     }
 }
